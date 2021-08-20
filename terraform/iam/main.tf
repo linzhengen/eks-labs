7756efa03667e7496d6d02ad4acfde80386a1d74
linzhengen <linzhengen@yahoo.co.jp>
2021-08-20 15:40:22 +0900

    add iam role

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -2,4 +2,22 @@ provider "aws" {
   region = var.region
 }
 
-// todo
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 3.0"
+
+  trusted_role_arns = [
+    "ec2.amazonaws.com",
+  ]
+
+  create_role = true
+
+  role_name         = "node-iam-role"
+  role_requires_mfa = false
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+  ]
+}
